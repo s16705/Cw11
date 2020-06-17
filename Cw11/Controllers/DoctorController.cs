a9958320acb4a012db4f508de5618f9aed7a1bbe
s16705 <61659821+s16705@users.noreply.github.com>
2020-06-17 23:27:42 +0200

    Initial commit

---
+++ Cw11/Controllers/DoctorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw11.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw11.Controllers
+{
+    [Route("api/doctor")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+
+        private readonly DoctorDbContext _context;
+
+        public DoctorController(DoctorDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetDoctor()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Cw11/Models/DoctorDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw11.Models
+{
+    public class DoctorDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctor { get; set; }
+        public DoctorDbContext() { }
+        public DoctorDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
